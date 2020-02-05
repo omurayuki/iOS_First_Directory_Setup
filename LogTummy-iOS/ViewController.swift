d5bd0dfb9c4950c73094e993c7485ba6d12a3d16
omurayuki <g1221438qz12@gmail.com>
2020-02-05 14:55:46 +0900

    初期プロジェクト+git作成

---
+++ LogTummy-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogTummy-iOS
+//
+//  Created by オムラユウキ on 2020/02/05.
+//  Copyright © 2020 オムラユウキ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LogTummy-iOSTests/LogTummy_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  LogTummy_iOSTests.swift
+//  LogTummy-iOSTests
+//
+//  Created by オムラユウキ on 2020/02/05.
+//  Copyright © 2020 オムラユウキ. All rights reserved.
+//
+
+import XCTest
+@testable import LogTummy_iOS
+
+class LogTummy_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
