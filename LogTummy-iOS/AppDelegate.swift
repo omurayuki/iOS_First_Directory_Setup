35a415fde4d1db23d121785de16220f3fb3272af
omurayuki <g1221438qz12@gmail.com>
2020-02-07 15:01:38 +0900

    Firebase 基本的なフレームワーク導入

--- LogTummy-iOS/AppDelegate.swift
+++ LogTummy-iOS/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        #if DEBUG
+            let filePath = Bundle.main.path(forResource: "GoogleService-debug-Info", ofType: "plist")
+        #else
+            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+        #endif
+        let options = FirebaseOptions(contentsOfFile: filePath!)
+        FirebaseApp.configure(options: options!)
+        
         return true
     }
 
@@ -34,4 +43,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
