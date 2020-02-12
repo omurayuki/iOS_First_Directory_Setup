c3a6be342735cbfb162018fb971fef6b0a25019a
omurayuki <g1221438qz12@gmail.com>
2020-02-12 20:28:28 +0900

    Firebase関連コンポーネント再導入

--- LogTummy-iOS/AppDelegate.swift
+++ LogTummy-iOS/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,13 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        #if DEBUG
-//            let filePath = Bundle.main.path(forResource: "GoogleService-debug-Info", ofType: "plist")
-//        #else
-//            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
-//        #endif
-//        let options = FirebaseOptions(contentsOfFile: filePath!)
-//        FirebaseApp.configure(options: options!)
+        // Override point for customization after application launch.
+        #if DEBUG
+            let filePath = Bundle.main.path(forResource: "GoogleService-debug-Info", ofType: "plist")
+        #else
+            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+        #endif
+        let options = FirebaseOptions(contentsOfFile: filePath!)
+        FirebaseApp.configure(options: options!)
         
         return true
     }
@@ -29,15 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
