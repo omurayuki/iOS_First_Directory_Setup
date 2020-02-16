04984aa028001b13359279c85049832f6064a7f3
omurayuki <g1221438qz12@gmail.com>
2020-02-16 16:54:33 +0900

    utilityファイル 空白調整

--- LogTummy-iOS/LegacyUIKit/Util/NumberFormatter+Util.swift
+++ LogTummy-iOS/LegacyUIKit/Util/NumberFormatter+Util.swift
@@ -4,7 +4,7 @@ public extension NumberFormatter {
 
     func formatStringAsInteger(from: String) -> String {
         guard let int = Int(from),
-        let string = string(from: NSNumber(value: int))else {
+        let string = string(from: NSNumber(value: int)) else {
             return ""
         }
         return string
