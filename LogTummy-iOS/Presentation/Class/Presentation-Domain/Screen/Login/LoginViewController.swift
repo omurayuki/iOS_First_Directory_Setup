1330e32b91f9b1f37e4556e8b2e89f5a3f153ad8
omurayuki <g1221438qz12@gmail.com>
2020-02-18 19:19:02 +0900

    Swifter link

--- LogTummy-iOS/Presentation/Class/Presentation-Domain/Screen/Login/LoginViewController.swift
+++ LogTummy-iOS/Presentation/Class/Presentation-Domain/Screen/Login/LoginViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwifteriOS
 
 class LoginViewController: UIViewController {
     
@@ -6,7 +7,14 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var LoginButton: CornerRoundableButton!
     
+    let swifter = Swifter(consumerKey: "", consumerSecret: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        swifter.authorize(withCallback: <#T##URL#>, presentingFrom: <#T##UIViewController?#>, success: { (<#Credential.OAuthAccessToken?#>, <#URLResponse#>) in
+//            <#code#>
+//        }) { (<#Error#>) in
+//            <#code#>
+//        }
     }
 }
