a2011d8e35f15525dd96e7d1bcfefac105c88024
omurayuki <g1221438qz12@gmail.com>
2020-02-24 22:41:35 +0900

    loginRemote作成

---
+++ LogTummy-iOS/Presentation/Class/Utility/SwifterGeneral.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum SwifterGeneral: CustomStringConvertible {
+    
+    case TWITTER_CONSUMER_KEY
+    case TWITTER_CONSUMER_SECRET
+    case CALLBACK_URL
+    
+    var description: String {
+        switch self {
+        case .TWITTER_CONSUMER_KEY:
+            return "sqOi4qeOAFWf9kJwNw5RkveM6"
+        case .TWITTER_CONSUMER_SECRET:
+            return "UtYIzFwCtb2fpJvUw4FYI6CJ8dgDqRGZ7NjCp1CdXz0pfjxIBP"
+        case .CALLBACK_URL:
+            return "swifter-sqOi4qeOAFWf9kJwNw5RkveM6://"
+        }
+    }
+}
