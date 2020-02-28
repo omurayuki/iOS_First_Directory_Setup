9229a8a1e975cbf2d754e1515958c57c4a8a9058
omurayuki <g1221438qz12@gmail.com>
2020-02-28 16:29:11 +0900

    storyboard レイアウト調整

--- LogTummy-iOS/Presentation/Class/Presentation-Domain/Screen/Launch/LaunchViewController.swift
+++ LogTummy-iOS/Presentation/Class/Presentation-Domain/Screen/Launch/LaunchViewController.swift
@@ -1,11 +1,3 @@
-//
-//  LaunchViewController.swift
-//  LogTummy-iOS
-//
-//  Created by オムラユウキ on 2020/02/14.
-//  Copyright © 2020 オムラユウキ. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
